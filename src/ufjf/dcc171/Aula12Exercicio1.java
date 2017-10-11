83c1056d188f4e4860f24e9ad4cf216937186acf
ice <ice@lab06m26.nrc.ice.ufjf.br>
2017-10-10 22:16:54 -0300

    Aula 12 - Exercico 1 - Início

---
+++ src/ufjf/dcc171/Aula12Exercicio1.java
@@ -0,0 +1,11 @@
+package ufjf.dcc171;
+
+public class Aula12Exercicio1 {
+
+    public static void main(String[] args) {
+        Janela j = new Janela();
+        j.setLocationRelativeTo(null);
+        j.setVisible(true);
+    }
+
+}
